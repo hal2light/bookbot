ae479a708f5fcff31b070309d6b9f2285f835d84
hal2light <halitberken09@gmail.com>
2025-12-10 17:04:03 +0300

    finished the project

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from stats import get_book_text
 from stats import get_num_words
 from stats import count_chars
-def main():
-    print(count_chars())
+from stats import sort_chars
+import sys
 
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    text = get_book_text(path)
+    word_count = get_num_words(text)
+    char_count_list = sort_chars(text)
+    char_count = ""
+    for dic in char_count_list:
+        char_count += f"{dic["char"]}: {dic["num"]}\n"
+
+    print(f'''
+============ BOOKBOT ============
+Analyzing book found at {path}...
+----------- Word Count ----------
+{word_count}
+--------- Character Count -------
+{char_count[:-1]}
+============= END ===============''')
 main()
     
 
--- stats.py
+++ stats.py
@@ -3,23 +3,23 @@ def get_book_text(path):
         file_contents = f.read()
         return file_contents
 
-def get_num_words():
-    text = get_book_text("./books/frankenstein.txt")
+def get_num_words(text):
     number_of_words = len(text.split())
     return f"Found {number_of_words} total words"
 
-def count_chars():
-    text = get_book_text("./books/frankenstein.txt")
+def count_chars(text):
     lowered_text = text.lower()
-    char_list = ["t","p","c"]
-    chars = {}
+    chars = []
+    char_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","æ","â","ê","ë","ô"]
     for char in char_list:
         char_count = lowered_text.count(char)
-        chars[char] = char_count
+        chars.append({"char":char,"num":char_count})
     return chars
 
-
-
-
-
+def sort_chars(text):
+    def sort_on(items):
+        return items["num"]
+    chars = count_chars(text)
+    chars.sort(reverse=True,key=sort_on)
+    return chars
 
